f8a135876e53e1b7d5f9b1580b2c769d982399ea
Goodnight-byte <53957618+Goodnight-byte@users.noreply.github.com>
2021-12-09 16:41:15 -0800

    Added class files and edit screen

---
+++ app/src/main/java/com/example/trackxpense/NewCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.trackxpense;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_category);
+    }
+}
