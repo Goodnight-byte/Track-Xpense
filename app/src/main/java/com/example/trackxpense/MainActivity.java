4d7c0fe6c051c0b9cd2eeb2d5ef1725570f3680e
Goodnight-byte <53957618+Goodnight-byte@users.noreply.github.com>
2021-12-09 20:08:46 -0800

    Implemented creation of Expense objects using the new expense activity

--- app/src/main/java/com/example/trackxpense/MainActivity.java
+++ app/src/main/java/com/example/trackxpense/MainActivity.java
@@ -6,8 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        ArrayList[] expenseList = new ArrayList[]{};
     }
     private Context getContext() {
         return this;
--- app/src/main/java/com/example/trackxpense/NewExpenseActivity.java
+++ app/src/main/java/com/example/trackxpense/NewExpenseActivity.java
@@ -1,8 +1,17 @@
 package com.example.trackxpense;
 
+import static java.lang.Float.parseFloat;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NewExpenseActivity extends AppCompatActivity {
 
@@ -10,5 +19,25 @@ public class NewExpenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_expense);
+    }
+
+    public void newExpense(View view) throws ParseException {
+
+        Switch s = (Switch) findViewById(R.id.recurringSwitch);
+        Boolean recurring;
+        if (s != null) {
+            recurring = true;
+        } else {
+            recurring = false;
+        }
+        EditText value = (EditText) findViewById(R.id.expenseFloat);
+        EditText date = (EditText) findViewById(R.id.expenseDate);
+        EditText category = (EditText) findViewById(R.id.expenseCategory);
+        String expenseValue = value.getText().toString();
+        String expenseDate = date.getText().toString();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date d = dateFormat.parse(expenseDate);
+        String expenseCategory = category.getText().toString();
+        Expense e = new Expense(parseFloat(expenseValue), d, recurring,expenseCategory);
     }
 }
