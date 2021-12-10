0dca45c0b4faf40f8e5d0d81db5095a6d790ab8c
Goodnight-byte <53957618+Goodnight-byte@users.noreply.github.com>
2021-12-09 19:20:48 -0800

    Added activities to manifest, fixed new expense button

--- app/src/main/java/com/example/trackxpense/MainActivity.java
+++ app/src/main/java/com/example/trackxpense/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+
     }
-    public void createExpense( View view) {
+
+    public void createExpense(View view) {
         Intent intent = new Intent(this, NewExpenseActivity.class);
         startActivity(intent);
     }
